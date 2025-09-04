aa5cecb45c1223d7b4aeb3573bae58d26c27f0f7
xsalles <xs.salles@gmail.com>
2025-09-04 16:46:45 -0300

    feat: created getAll, getById, deleteById methods and now I'll add documentation

--- src/main/java/com/example/guincho/modules/Vehicle/controller/VehicleController.java
+++ src/main/java/com/example/guincho/modules/Vehicle/controller/VehicleController.java
@@ -3,7 +3,9 @@ package com.example.guincho.modules.Vehicle.controller;
 import com.example.guincho.modules.Vehicle.model.VehicleModel;
 import com.example.guincho.modules.Vehicle.services.VehicleService;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,13 +16,28 @@ public class VehicleController {
 	private VehicleService vehicleService;
 
 	@PostMapping("/create")
-	public VehicleModel createVehicle(@Valid @RequestBody VehicleModel vehicleModel) {
-		return vehicleService.cadastrarVeiculo(vehicleModel);
+	public ResponseEntity<String> createVehicle(@Valid @RequestBody VehicleModel vehicleModel) {
+		return vehicleService.registerVehicle(vehicleModel);
 	}
 
 	@PostMapping("/request-tow/{id}")
-	public String requestTow(@PathVariable Integer id) {
-		return vehicleService.solicitarGuincho(id);
+	public ResponseEntity<String> requestTow(@PathVariable Integer id) {
+		return vehicleService.requestTow(id);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> deleteVehicleById(@PathVariable Integer id) {
+		return vehicleService.deleteById(id);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getVehicleById(@PathVariable Integer id) {
+		return vehicleService.getById(id);
+	}
+
+	@GetMapping("/all")
+	public ResponseEntity<?> getAllVehicles() {
+		return vehicleService.getAll();
 	}
 
 }
--- src/main/java/com/example/guincho/modules/Vehicle/services/VehicleService.java
+++ src/main/java/com/example/guincho/modules/Vehicle/services/VehicleService.java
@@ -3,31 +3,56 @@ package com.example.guincho.modules.Vehicle.services;
 import com.example.guincho.modules.Vehicle.model.VehicleModel;
 import com.example.guincho.modules.Vehicle.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VehicleService {
 	@Autowired
 	private VehicleRepository vehicleRepository;
-	private VehicleModel vehicleModel;
 
-
-	public VehicleModel cadastrarVeiculo(VehicleModel vehicleModel) {
-
-		if (vehicleModel == null) {
-			throw new IllegalArgumentException("Please, complete all fields");
+	public ResponseEntity<?> getAll() {
+		if (vehicleRepository.findAll().isEmpty()) {
+			return new ResponseEntity<>("No content: No vehicles registered", HttpStatus.NO_CONTENT);
 		}
 
-		return vehicleRepository.save(vehicleModel);
+		return ResponseEntity.ok().body(vehicleRepository.findAll());
 	}
 
-	public String solicitarGuincho(Integer id) {
-		VehicleModel vehicle = vehicleRepository.getById(id);
-
-		if (vehicle == null) {
-			return "Vehicle não encontrado";
+	public ResponseEntity<?> getById(Integer id) {
+		if (!vehicleRepository.existsById(id)) {
+			return new ResponseEntity<>("Error: Vehicle not found", HttpStatus.NOT_FOUND);
 		}
 
-		return "Guincho solicitado para o veículo com placa: " + vehicle.getPlacaVeiculo() + " do cliente: " + vehicle.getNomeCliente() + ", telefone: " + vehicle.getTelefoneCelular() + ", endereço: " + vehicle.getEndereco() + ", cidade: " + vehicle.getCidade();
+		return new ResponseEntity<>(vehicleRepository.findById(id), HttpStatus.OK);
+	}
+
+	public ResponseEntity<String> registerVehicle(VehicleModel vehicleModel) {
+		if (vehicleModel == null) {
+			return new ResponseEntity<>("Please, provide vehicle data", HttpStatus.BAD_REQUEST);
+		}
+
+		vehicleRepository.save(vehicleModel);
+
+		return new ResponseEntity<>("Vehicle registered successfully", HttpStatus.CREATED);
+	}
+
+	public ResponseEntity<String> requestTow(Integer id) {
+		if (!vehicleRepository.existsById(id)) {
+			return new ResponseEntity<>("Error: Vehicle not found", HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<>("Tow truck requested successfully", HttpStatus.OK);
+	}
+
+	public ResponseEntity<String> deleteById(Integer id) {
+		if (!vehicleRepository.existsById(id)) {
+			return new ResponseEntity<>("Error: Vehicle not found", HttpStatus.NOT_FOUND);
+		}
+
+		vehicleRepository.deleteById(id);
+
+		return new ResponseEntity<>("Vehicle deleted sucessfully", HttpStatus.OK);
 	}
 }
